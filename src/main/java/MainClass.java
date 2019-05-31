8898020b7b3f6878fe88a9c56286d46b6ddb16d4
zuozhiw <zuozhiw@gmail.com>
2019-05-31 14:22:04 -0700

    flink q1 60G

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,8 +1,8 @@
 public class MainClass {
 
     public static void main(String[] args) throws Exception {
-//        TpchQ1.run(args);
-        TpchQ13.run(args);
+        TpchQ1.run(args);
+//        TpchQ13.run(args);
     }
 
 }
